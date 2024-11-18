8485165e35d722f9aedb8c4db584c82faa96af50
hetal <hetalvadadoriya23@gmail.com>
2024-11-18 22:27:02 +0530

    remove project from git

--- game/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from game.models import *
-# Register your models here.
-
-admin.site.register(Tournament)
-admin.site.register(Slot)
-admin.site.register(Participant)
